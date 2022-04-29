64cb161c4d3234f9ce5c4f75245bf9ba6b43f04c
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-04-30 00:54:02 +0600

    Products load and pagination added

--- index.js
+++ index.js
@@ -8,6 +8,52 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l5hhy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("emaJohn").collection("products");
+
+        app.get('/product', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            let product;
+            if (page || size) {
+                product = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                product = await cursor.toArray();
+            }
+            res.send(product);
+        })
+
+        app.get('/productCount', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const count = await cursor.count();
+            res.send({ count });
+        })
+
+        app.post('/productByKeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = { _id: { $in: ids } };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Server working');
 })
